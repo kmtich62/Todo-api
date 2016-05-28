4ee3a28f039c2465c93d127d5ba92712a88da6bd
Kevin Mitchell <kmitchell62@gmail.com>
2016-05-28 13:39:50 -0400

    adding underscore support

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -24,27 +25,30 @@ app.get('/todos', function(req, res){
 app.get('/todos/:id', function(req, res){
     var todoid = parseInt(req.params.id, 10);
     var foundItem;
-    todos.forEach(function(todo){
-       if(todo.id === todoid){
-           foundItem = todo;
-       }
-    });
 
+    foundItem = _.findWhere(todos, {id: todoid});
     if(foundItem){
         res.json(foundItem);
     } else {
         res.status(404).send();
     }
 });
+
 //POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if(!_.isString(body.description) || !_.isBoolean(body.completed) || body.description.trim().length === 0){
+        res.status(400).send();
+    }
 
     body.id = todoNextId;
+    body.description = body.description.trim();
     todoNextId++;
     todos.push(body);
 
     res.json(body);
+
 });
 
 app.listen(PORT, function(){
