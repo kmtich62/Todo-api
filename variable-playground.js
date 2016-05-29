5d66ed919b2767328af5881f0ba60ab8305b5429
Kevin Mitchell <kmitchell62@gmail.com>
2016-05-29 10:31:32 -0400

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,15 @@
+//passing variables by reference
+var person = {
+    name: 'andrew',
+    age: 21
+}
+
+function updatePerson(obj){
+    
+}
+
+var grades = [15, 98];
+
+function addGrades(grades){
+    grades.push(55);
+}
