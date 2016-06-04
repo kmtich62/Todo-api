87029a05152a5c940e2bae03d1f2ac9346072c50
Kevin Mitchell <kmitchell62@gmail.com>
2016-06-04 17:25:23 -0400

    add user model and post users request

--- db.js
+++ db.js
@@ -18,17 +18,10 @@ if(env==='production'){
     });
 }
 
-
-
-//
-// var sequelize = new Sequelize(undefined, undefined, undefined, {
-//     'dialect': 'sqlite',
-//     'storage': __dirname + '/data/dev-todo-api.sqlite'
-// });
-
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.user = sequelize.import(__dirname + '/models/user.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
